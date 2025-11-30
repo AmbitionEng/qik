7dca4cd67f0db1928fd7c1328dc857d21f3facf7
Wes Kendall <wesleykendall@gmail.com>
2025-11-30 17:32:15 -0600

    Drop Python 3.9 support, add Python 3.14 support. (#20)
    * Initialize template from version b7d2321846eeeb120ea9455597ddcf9bd8b5e7a4
    
    * Update template to version baafae2af9d39e491ca17b7fd197562883252ceb
    
    * Drop Python 3.9 support, add Python 3.14 support.
    
    * Fix requirements
    
    * Fix issues
    
    * fix coverage
    
    * 0.2.7 release

--- qik/tests/test_hash.py
+++ qik/tests/test_hash.py
@@ -50,9 +50,9 @@ def test_globs(tmpdir, mocker):
 
 def test_pydists():
     venv = qik.venv.active()
-    assert qik.hash.pydists("pytest", venv=venv) == "7113a7e1bfec3488e5ebce346daa3b06"
+    assert qik.hash.pydists("pytest", venv=venv) == "07178c065927b00d5a2f349755548925"
     assert (
-        qik.hash.pydists("pytest", "pytest-cov", venv=venv) == "21e01cb189bd6bad834a346ba065da5f"
+        qik.hash.pydists("pytest", "pytest-cov", venv=venv) == "7f553629ffbb74d5a7a695faaf0043ad"
     )
 
 
